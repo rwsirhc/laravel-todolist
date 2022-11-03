9bf6ace29f10dbcec0658999ee0eecfbaeff77f9
chwlr <ch.williamr@gmail.com>
2022-11-04 01:45:57 +0800

    add user service

---
+++ app/Services/Impl/UserServiceImpl.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services\Impl;
+use App\Services\UserService;
+
+class UserServiceImpl implements UserService
+{
+
+	/**
+	 *
+	 * @param string $email
+	 * @param string $password
+	 *
+	 * @return bool
+	 */
+	function login(string $email, string $password): bool 
+  {
+    
+	}
+}
